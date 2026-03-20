bd2ac3087bc4df9c036d6a120541066db88fa4e5
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-03-19 22:15:01 -0300

    feat(users): add UserSerializer for user model
    Introduce a ModelSerializer that exposes all fields of the User model for API usage.

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'last_login',
+            'date_joined',
+        ]
