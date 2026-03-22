19929faae2d809847438cca83527591c0c3954ef
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-03-22 11:23:37 -0300

    feat(permissions): add global default permission class
    Introduce GlobalDefaulPermission class to enforce model-level permissions based on HTTP method and view queryset. This replaces simple IsAuthenticated checks in user and product views, enabling fine-grained access control using Django's permission system.

--- products/views.py
+++ products/views.py
@@ -1,34 +1,36 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from core.permissions.default import GlobalDefaulPermission
 from products.serializers import ProductSerializer, ProductPlatformSerializer, PriceAlertSerializer
 from products.models import Product, ProductPlatform, PriceAlert
 
 class ProductListCreateView(ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaulPermission]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaulPermission]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductPlatformListCreateView(ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaulPermission]
     queryset = ProductPlatform.objects.all()
     serializer_class = ProductPlatformSerializer
 
 class ProductPlatformRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaulPermission]
     queryset = ProductPlatform.objects.all()
     serializer_class = ProductPlatformSerializer
 
 class PriceAlertListCreateView(ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaulPermission]
     queryset = PriceAlert.objects.all()
     serializer_class = PriceAlertSerializer
 
 class PriceAlertRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaulPermission]
     queryset = PriceAlert.objects.all()
     serializer_class = PriceAlertSerializer
